745e5f81e6dfc089ae76dba96a3393a072cd8922
Jenied <C22380473@mytudublin.ie>
2025-02-13 21:07:46 +0000

    added program to calculate average

--- lab2.py
+++ lab2.py
@@ -4,3 +4,8 @@ print('This is another string!')
 for i in range (10):
     print(i+1)
 
+# Compute the average of a set of numbers
+numbers = (input("Enter a set of numbers (separated by spaces): "))
+numbers = list(map(float, numbers.split()))
+average = sum(numbers) / len(numbers)
+print(f"The average is: {average} ")
